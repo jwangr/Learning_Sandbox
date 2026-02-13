68bb720fd028865869d69e2596147b855dde4188
Regina-Wang_cba <regina.wang@cba.com.au>
2026-02-13 13:05:14 +1100

    Added http headers

--- CoreEmpty/Program.cs
+++ CoreEmpty/Program.cs
@@ -1,6 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); // configures builder
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async (HttpContext context) =>
+{
+    context.Response.Headers["myKey"] = "myValue"; // add Http Header Values
+    context.Response.Headers["myServer"] = "my Kestrel Server";
+    await context.Response.WriteAsync("Hello "); // Writes into body
+    await context.Response.WriteAsync("First Udemy Course");
 
-app.Run();
+});
+
+app.Run(); // starts the server (default application server = Kestrel, returns information to the client's browser) with 'dotnet run'
