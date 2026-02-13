67c2f8f5c2935a729045e684d76a62ebcf51b87a
Regina-Wang_cba <regina.wang@cba.com.au>
2026-02-13 13:36:43 +1100

    Middlewares are chronological

--- CoreEmpty/Program.cs
+++ CoreEmpty/Program.cs
@@ -1,13 +1,20 @@
 var builder = WebApplication.CreateBuilder(args); // configures builder
 var app = builder.Build();
 
+// Middleware runs in chronological order
+// Request Delegate enables it to run to the next middleware
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello ");
+    await next(context); // call the next middleware with this "context"
+});
+
 app.MapGet("/", async (HttpContext context) =>
 {
     context.Response.Headers["myKey"] = "myValue"; // add Http Header Values
     context.Response.Headers["myServer"] = "my Kestrel Server";
-    await context.Response.WriteAsync("Hello "); // Writes into body
     await context.Response.WriteAsync("First Udemy Course");
 
 });
 
-app.Run(); // starts the server (default application server = Kestrel, returns information to the client's browser) with 'dotnet run'
+app.Run();
